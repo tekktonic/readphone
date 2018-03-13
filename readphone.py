4eb197ba3574dafd4eca3db91e66306da5c7dc71
Daniel Wilkins Daniel.Wilkins@kpit.com <Daniel.Wilkins@kpit.com>
2018-03-13 10:51:33 -0400

    Fix all pylint errors.

--- readphone.py
+++ readphone.py
@@ -1,13 +1,13 @@
-"""Interpret AndroSensor data into various 'interesting' categories and metrics, depending on your definition of interesting."""
+"""Interpret AndroSensor data into various 'interesting'
+categories and metrics, depending on your definition of interesting."""
 
-# CSV import things
+# CSV import
 import pandas as p
 
 # The function-based interface for plotting lists.
 import matplotlib.pyplot as plot
 
-# Python standard math library.
-import math
+
 
 # These are the various offsets of interesting measurements in AndroSensor data.
 ACCEL_X_INDEX = 0
@@ -32,16 +32,18 @@ TIME_INDEX = 29
 def column(arr, col):
     """Given a 2D array, extract all the elements in one column to its own list"""
     ret = []
-    for i in range(0, len(arr)):
-        ret.append(arr[i][col])
+    for _, item in enumerate(arr):
+        ret.append(item[col])
     return ret
 
 def is_low_light(frame):
-    """Microsoft's usability guidelines say that dim lighting starts at 200 lux."""
-    return (frame[LUX_INDEX] < 200)
+    """Microsoft's usability guidelines say that dim
+    lighting starts at 200 lux."""
+    return frame[LUX_INDEX] < 200
 
 def is_close(frame):
-    """My particular phone sensor is binary, either 5cm or 0cm. I can't vouch for others."""
+    """My particular phone sensor is binary, either 5cm or 0cm.
+    I can't vouch for others."""
     return frame[PROXIMITY_INDEX] < 5
 
 def is_audible(frame):
@@ -49,91 +51,92 @@ def is_audible(frame):
     return frame[DECIBEL_INDEX] > 60
 
 def is_active(frame):
-    """Just an arbitrary threshhold for "Yeah, it looks like this person is moving" via the sensors"""
+    """Just an arbitrary threshhold for "Yeah, it looks like this person is moving"
+    via the sensors"""
     return (abs(frame[LAT_X_INDEX]) + abs(frame[LAT_Y_INDEX]) + abs(frame[LAT_Z_INDEX])) > 1
 
 
 # Read the CSV file containing all of our data
-data = p.read_csv("data.csv")
+DATA = p.read_csv("data.csv")
 
 # Extract the numpy array which is the actual data values without context.
-num = data.values
+NUM = DATA.values
 
 
 # The time column is the X axis for practically every graph
-time = column(num, TIME_INDEX)
+TIME = column(NUM, TIME_INDEX)
 
 # Time is recorded in milliseconds, convert to seconds.
-time = [time / 1000.0 for time in time]
+TIME = [TIME / 1000.0 for TIME in TIME]
 
 # Extract all of our sensor snapshots which are in low light.
-low_light_frames = [frame for frame in num if is_low_light(frame)]
+LOW_LIGHT_FRAMES = [frame for frame in NUM if is_low_light(frame)]
 
-# If the light is low and there's something right next to us, we're probably either on the phone or it's in the pocket.
-pocket = [frame for frame in low_light_frames if is_close(frame)]
+# If the light is low and there's something right next to us,
+#we're probably either on the phone or it's in the pocket.
+POCKET = [frame for frame in LOW_LIGHT_FRAMES if is_close(frame)]
 
 
 # Extract all frames which were above the conversation threshhold
-noisy = [frame for frame in pocket if is_audible(frame)]
+NOISY = [frame for frame in POCKET if is_audible(frame)]
 
 
-print("There are " + str(len(num)) + " frames of data in total, each frame is 0.5s")
+print("There are " + str(len(NUM)) + " frames of data in total, each frame is 0.5s")
 
-print("There are " + str(len(low_light_frames)) + " frames with low light")
+print("There are " + str(len(LOW_LIGHT_FRAMES)) + " frames with low light")
 
-print("Of those, it was probably in my pocket " + str(len(pocket)) + " frames");
+print("Of those, it was probably in my pocket " + str(len(POCKET)) + " frames")
 
-print("The average light level overall was " + str(sum(column(num, LUX_INDEX))/len(num)) + ". I'm in a dark part of the office.")
-
+print("The average light level overall was "
+      + str(sum(column(NUM, LUX_INDEX))/len(NUM)) + ". I'm in a dark part of the office.")
 
 # Just get the total time from the first and last frames quickly.
-total_time = (num[-1][TIME_INDEX] - num[0][TIME_INDEX]) / 1000.0
-
+TOTAL_TIME = (NUM[-1][TIME_INDEX] - NUM[0][TIME_INDEX]) / 1000.0
 
 # Pull all frames during which we were active (above a linear acceleration threshhold.)
-active_time = [frame for frame in num if is_active(frame)]
+ACTIVE_TIME = [frame for frame in NUM if is_active(frame)]
 
+print("Of those frames when it was probably in my pocket, we probably were able to pick "
+      + "up on conversation for " + str(len(NOISY)) + " of them.")
 
-print("Of those frames when it was probably in my pocket, we probably were able to pick up on conversation for " + str(len(noisy)) + " of them.")
-
-print("The total amount of time spent recording was " + str(total_time) + " S")
+print("The total amount of time spent recording was " + str(TOTAL_TIME) + " S")
 
 # Each frame is 500 milliseconds.
-active_time = (500 * len(active_time)) / 1000.0
+ACTIVE_TIME = (500 * len(ACTIVE_TIME)) / 1000.0
 
-print("The active time was " + str(active_time) + ".")
+print("The active time was " + str(ACTIVE_TIME) + ".")
 
-print("Which means I had an active percentage of " + str((active_time / total_time) * 100) + ".")
-
+print("Which means I had an active percentage of " + str((ACTIVE_TIME / TOTAL_TIME) * 100) + ".")
 
 # Plot various relevant metrics.
-
-plot.plot(time, column(num, LUX_INDEX))
+plot.plot(TIME, column(NUM, LUX_INDEX))
 plot.title("Lux (light level) vs time.")
 plot.show()
 
-plot.plot(time, column(num,DECIBEL_INDEX))
+plot.plot(TIME, column(NUM, DECIBEL_INDEX))
 plot.title("Decibels (sound level) vs time.")
 plot.show()
 
 # Take every frame and sum the X, Y, and Z acceleration magnitudes in those frames.
-movement_sums = []
-for frame in num:
-    movement_sums.append((abs(frame[LAT_X_INDEX]) + abs(frame[LAT_Y_INDEX]) + abs(frame[LAT_Z_INDEX])))
-    
-plot.plot(time, movement_sums)
+MOVEMENT_SUMS = []
+for sensor_frame in NUM:
+    MOVEMENT_SUMS.append((abs(sensor_frame[LAT_X_INDEX]) + abs(sensor_frame[LAT_Y_INDEX])
+                          + abs(sensor_frame[LAT_Z_INDEX])))
+
+plot.plot(TIME, MOVEMENT_SUMS)
 plot.title("Total movement vs time.")
 plot.show()
 
-# Just show a binary chart: either the user is active or the user is not, as determined by our threshhold that we decided on before.
-active = []
-for frame in num:
-    if ((abs(frame[LAT_X_INDEX]) + abs(frame[LAT_Y_INDEX]) + abs(frame[LAT_Z_INDEX])) > 1):
-        active.append(1)
+# Just show a binary chart: either the user is active or the user is not,
+# as determined by our threshhold that we decided on before.
+ACTIVE = []
+for sensor_frame in NUM:
+    if (abs(sensor_frame[LAT_X_INDEX]) + abs(sensor_frame[LAT_Y_INDEX])
+            + abs(sensor_frame[LAT_Z_INDEX])) > 1:
+        ACTIVE.append(1)
     else:
-        active.append(0)
-    
-plot.bar(time, active)
+        ACTIVE.append(0)
+
+plot.bar(TIME, ACTIVE)
 plot.title("Blocks of active time.")
 plot.show()
-
